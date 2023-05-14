9d4aa14234379663b45d9088dffa2f1903b23d64
abdul-kader715 <you@example.com>
2023-05-14 19:24:24 +0600

    log page

--- assets/js/script.js
+++ assets/js/script.js
@@ -207,24 +207,24 @@ $(function () {
 
 /* images-show */
 
-$(document).ready(function() {
+$(document).ready(function () {
   if (window.File && window.FileList && window.FileReader) {
-    $("#files").on("change", function(e) {
+    $("#files").on("change", function (e) {
       var files = e.target.files,
         filesLength = files.length;
       for (var i = 0; i < filesLength; i++) {
         var f = files[i]
         var fileReader = new FileReader();
-        fileReader.onload = (function(e) {
+        fileReader.onload = (function (e) {
           var file = e.target;
           $("<span class=\"pip\">" +
             "<img class=\"imageThumb\" src=\"" + e.target.result + "\" title=\"" + file.name + "\"/>" +
             "<br/><span class=\"remove\">Remove image</span>" +
             "</span>").insertAfter("#files");
-          $(".remove").click(function(){
+          $(".remove").click(function () {
             $(this).parent(".pip").remove();
           });
-          
+
         });
         fileReader.readAsDataURL(f);
       }
@@ -232,5 +232,33 @@ $(document).ready(function() {
     });
   } else {
     alert("Your browser doesn't support to File API")
+  }
+});
+
+$(document).ready(function () {
+  $('.input').focus(function () {
+    $(this).parent().find(".label-txt").addClass('label-active');
+  });
+  $(".input").focusout(function () {
+    if ($(this).val() == '') {
+      $(this).parent().find(".label-txt").removeClass('label-active');
+    };
+  });
+});
+
+
+
+
+
+
+
+$(".toggle-password").click(function () {
+
+  $(this).toggleClass("fa fa-eye fa fa-eye-slash");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
   }
 });
